e1d810f02f6dbbe00e581256fb9d18054993da9e
Alfi Azizi <azizi.alfi15@gmail.com>
2020-05-17 19:08:17 +0700

    Menyiapkan Base Component

---
+++ src/components/molecules/DoctorCategory/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const DoctorCategory = () => {
+  return (
+    <View>
+      <Text>DoctorCategory Component</Text>
+    </View>
+  );
+};
+
+export default DoctorCategory;
+
+const styles = StyleSheet.create({});
