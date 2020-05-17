f87cbcea9f4136eed9ebdbea9ed7e0203539cd50
Alfi Azizi <azizi.alfi15@gmail.com>
2020-05-17 17:27:23 +0700

    Setup Bottom Tab Navigator

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import {Splash, GetStarted, Register, Login, UploadPhoto} from '../pages';
+import {
+  Splash,
+  GetStarted,
+  Register,
+  Login,
+  UploadPhoto,
+  Doctor,
+  Hospitals,
+  Messages,
+} from '../pages';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainApp = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Doctor" component={Doctor} />
+      <Tab.Screen name="Messages" component={Messages} />
+      <Tab.Screen name="Hospitals" component={Hospitals} />
+    </Tab.Navigator>
+  );
+};
 
 const Router = () => {
   return (
@@ -32,6 +53,7 @@ const Router = () => {
         component={UploadPhoto}
         options={{headerShown: false}}
       />
+      <Stack.Screen name="MainApp" component={MainApp} />
     </Stack.Navigator>
   );
 };
