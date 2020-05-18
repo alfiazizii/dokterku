3e66bacc8ab22beed06084296d845454487557a8
Alfi Azizi <azizi.alfi15@gmail.com>
2020-05-18 17:54:48 +0700

    Styling Chat Text Input

--- src/pages/Chatting/index.js
+++ src/pages/Chatting/index.js
@@ -5,12 +5,14 @@ import {fonts, colors} from '../../utils';
 
 const Chatting = () => {
   return (
-    <View>
+    <View style={styles.page}>
       <Header type="dark-profile" title="Putri Hayza" />
-      <Text style={styles.chatDate}>Senin, 18 Mei 2020</Text>
-      <ChatItem />
-      <ChatItem />
-      <ChatItem />
+      <View style={styles.content}>
+        <Text style={styles.chatDate}>Senin, 18 Mei 2020</Text>
+        <ChatItem />
+        <ChatItem />
+        <ChatItem />
+      </View>
       <InputChat />
     </View>
   );
@@ -19,6 +21,13 @@ const Chatting = () => {
 export default Chatting;
 
 const styles = StyleSheet.create({
+  page: {
+    backgroundColor: colors.white,
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+  },
   chatDate: {
     fontSize: 11,
     fontFamily: fonts.primary.normal,
