fb70e6396685879e8412d4865f06ac0e67a8c39e
Alfi Azizi <azizi.alfi15@gmail.com>
2020-05-23 13:43:58 +0700

    Menambahkan Setup Firebase

---
+++ src/config/Firebase/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyAF9Oa9NQWrBOPEv64nH6rDhjgEgxmN5k8',
+  authDomain: 'app-dokterku.firebaseapp.com',
+  databaseURL: 'https://app-dokterku.firebaseio.com',
+  projectId: 'app-dokterku',
+  storageBucket: 'app-dokterku.appspot.com',
+  messagingSenderId: '323119838942',
+  appId: '1:323119838942:web:67bab494a14f731bedb44f',
+});
+
+const Firebase = firebase;
+
+export default Firebase;
