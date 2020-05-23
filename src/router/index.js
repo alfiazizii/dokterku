825a5753e26b8fb224bcd758d03d6487c00e1f5b
Alfi Azizi <azizi.alfi15@gmail.com>
2020-05-23 17:02:44 +0700

    Menambahkan Logic kedalam Icon Add & Remove Photo Menambahkan Fungsi Upload Photo dari Gallery Handle Error Ketitka User Gagal Upload Photo

--- src/router/index.js
+++ src/router/index.js
@@ -33,7 +33,7 @@ const MainApp = () => {
 
 const Router = () => {
   return (
-    <Stack.Navigator initialRouteName={Splash}>
+    <Stack.Navigator initialRouteName="Splash">
       <Stack.Screen
         name="Splash"
         component={Splash}
