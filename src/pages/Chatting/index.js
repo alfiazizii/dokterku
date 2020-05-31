e5784fcc5e69c37ddc7160856771f849e34e86bf
Alfi Azizi <azizi.alfi15@gmail.com>
2020-05-31 13:49:20 +0700

    Mengambil Data Chat dari Firebase

--- src/pages/Chatting/index.js
+++ src/pages/Chatting/index.js
@@ -15,13 +15,46 @@ const Chatting = ({navigation, route}) => {
   const dataDoctor = route.params;
   const [chatContent, setChatContent] = useState('');
   const [user, setUser] = useState({});
+  const [chatData, setChatData] = useState([]);
 
   useEffect(() => {
+    getDataUserFromLocal();
+    const chatId = `${user.uid}_${dataDoctor.data.uid}`;
+    const urlFirebase = `chatting/${chatId}/allChat/`;
+    Firebase.database()
+      .ref(urlFirebase)
+      .on('value', snapshot => {
+        console.log('data chat: ', snapshot.val());
+        if (snapshot.val()) {
+          const dataSnapshot = snapshot.val();
+          const allDataChat = [];
+          Object.keys(dataSnapshot).map(key => {
+            const dataChat = dataSnapshot[key];
+            const newDataChat = [];
+
+            Object.keys(dataChat).map(itemChat => {
+              newDataChat.push({
+                id: itemChat,
+                data: dataChat[itemChat],
+              });
+            });
+
+            allDataChat.push({
+              id: key,
+              data: newDataChat,
+            });
+          });
+          console.log('all data chat: ', allDataChat);
+          setChatData(allDataChat);
+        }
+      });
+  }, [dataDoctor.data.uid, user.uid]);
+
+  const getDataUserFromLocal = () => {
     getData('user').then(res => {
-      console.log('user login: ', res);
       setUser(res);
     });
-  }, []);
+  };
 
   const chatSend = () => {
     const today = new Date();
@@ -37,8 +70,6 @@ const Chatting = ({navigation, route}) => {
 
     const urlFirebase = `chatting/${chatId}/allChat/${setDateChate(today)}`;
 
-    console.log('data untuk di kirim: ', data);
-    console.log('url firebase: ', urlFirebase);
     Firebase.database()
       .ref(urlFirebase)
       .push(data)
@@ -60,10 +91,23 @@ const Chatting = ({navigation, route}) => {
       />
       <View style={styles.content}>
         <ScrollView showsVerticalScrollIndicator={false}>
-          <Text style={styles.chatDate}>Senin, 18 Mei 2020</Text>
-          <ChatItem isMe />
-          <ChatItem />
-          <ChatItem isMe />
+          {chatData.map(chat => {
+            return (
+              <View key={chat.id}>
+                <Text style={styles.chatDate}>{chat.id}</Text>
+                {chat.data.map(itemChat => {
+                  return (
+                    <ChatItem
+                      key={itemChat.id}
+                      isMe={itemChat.data.sendBy === user.uid}
+                      text={itemChat.data.chatContent}
+                      date={itemChat.data.chatTime}
+                    />
+                  );
+                })}
+              </View>
+            );
+          })}
         </ScrollView>
       </View>
       <InputChat
